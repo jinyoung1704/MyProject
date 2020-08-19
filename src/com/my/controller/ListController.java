79cc139900eb77452656a59b2979777812ac0017
jinyoung1704 <jinyoung1704@naver.com>
2020-08-19 23:06:15 +0900

    사용환경 추가 및 add메소드 미완성

--- src/com/my/controller/ListController.java
+++ src/com/my/controller/ListController.java
@@ -5,12 +5,17 @@
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.my.list.IListDAO;
+import com.my.list.ListDTO;
 
   @Controller 
   public class ListController
@@ -18,11 +23,31 @@ import org.springframework.web.bind.annotation.RequestMethod;
 	  
 	// mybatis 객체 의존성 (자동) 주입을 위한 준비
 	@Autowired
+	private SqlSession SqlSession;
 	
 	@RequestMapping(value = "/addList.action", method = RequestMethod.GET)
 	public String cateList(Model model, String bid, HttpServletRequest request)
 	{
+		HttpSession session = request.getSession();
 		String view=null;
+		String name = request.getParameter("name");
+		String url = request.getParameter("url");
+		
+		try
+		{
+			IListDAO dao = SqlSession.getMapper(IListDAO.class);
+			
+			ListDTO dto = new ListDTO();
+			
+			dto.setName(name);
+			dto.setUrl(url);
+			
+			dao.addList(dto);
+			
+		} catch (Exception e)
+		{
+		}
+		
 		
 		view = "/index.jsp";
 				
--- src/com/my/list/IListDAO.java
+++ src/com/my/list/IListDAO.java
@@ -1,8 +1,7 @@
 package com.my.list;
 
-import java.util.ArrayList;
 
 public interface IListDAO
 {
-	public ArrayList<ListDTO> addList(ListDTO dto);
+	public void addList(ListDTO dto);
 }
